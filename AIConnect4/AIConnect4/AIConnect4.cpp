9fbf271e659f75b938675225bda1341b586412c1
toddgilc <124370846+toddgilc@users.noreply.github.com>
2024-12-13 19:04:56 +0000

    Adding files

---
+++ AIConnect4/AIConnect4/AIConnect4.cpp
@@ -0,0 +1,9 @@
+// AIConnect4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
