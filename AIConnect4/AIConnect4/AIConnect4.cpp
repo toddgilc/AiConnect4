a3e3acab59010ad23b3c8a2b83d208a93d12b449
toddgilc <2200811@uad.ac.uk>
2024-12-14 21:46:24 +0000

    Revert "check sides not working"
    This reverts commit fb8ecb00d8163cc9f04e253befc27c1d98ab1db2.

--- AIConnect4/AIConnect4/AIConnect4.cpp
+++ AIConnect4/AIConnect4/AIConnect4.cpp
@@ -27,7 +27,7 @@ int main()
     std::cout << "Hello World!\n";
     gameBoard.startGame();
     gameBoard.displayBoard();
-    int count = 0;
+
     do {
        /* std::cout << "AI is thinking...." << std::endl;
         int aiChoice = rand() % 7;
@@ -49,19 +49,12 @@ int main()
             gameOver = true;
             std::cout << "YELLOW WINS!" << std::endl;
             break;
-        }
-         //need to if check get possible mvoes
-*/     
-        if (count % 2 == 0)
-        {
-            gameBoard.setCurrentPlayer(1);
-        }
-        else { gameBoard.setCurrentPlayer(2); }
-
+        }*/
+        gameBoard.setCurrentPlayer(2);
         std::cout << "Players move...." << std::endl;
         bool validMove = true;
         int placement = -1;
-        count++;
+
         do {
 
             placement = -1;
@@ -77,19 +70,6 @@ int main()
         gameBoard.displayBoard();
 
 
-        BOARD_SQUARE_STATE winner = gameBoard.checkWin();
-
-        if (winner == BOARD_SQUARE_STATE::RED)
-        {
-            gameOver = true;
-            std::cout << "RED WINS!" << std::endl;
-        }
-        else if (winner == BOARD_SQUARE_STATE::YELLOW)
-        {
-            gameOver = true;
-            std::cout << "YELLOW WINS!" << std::endl;
-        }
-       
 
     } while (!gameOver);
 
